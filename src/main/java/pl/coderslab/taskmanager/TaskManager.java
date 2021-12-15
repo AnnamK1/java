40fb586e11a72715a4edff2d29051eb28193ce57
AnnamK1 <annam@kurzydlo.com>
2021-12-15 23:27:20 +0100

    displaying available options

--- src/main/java/pl/coderslab/taskmanager/TaskManager.java
+++ src/main/java/pl/coderslab/taskmanager/TaskManager.java
@@ -1,4 +1,24 @@
 package pl.coderslab.taskmanager;
 
+import pl.coderslab.ConsoleColors;
+
+import static jdk.internal.org.jline.utils.InfoCmp.Capability.tab;
+
 public class TaskManager {
+    static final String FILE_NAME = "tasks.csv";
+    static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+    static String [] [] tasks;
+
+    public static void printOptions(String[] tab) {
+        System.out.println(ConsoleColors.BLUE);
+        System.out.println("Please select an option: " + ConsoleColors.RESET);
+        for (String option : tab) {
+            System.out.println(option);
+        }
+    }
+
+    public static void main (String[] args) {
+        printOptions(OPTIONS);
+
+    }
 }
